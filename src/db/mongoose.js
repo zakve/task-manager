73a107c35fc534f61ae3bc853a2e8cbbd6956be3
zakve <zaklas.martin@gmail.com>
2022-01-18 20:18:12 +0100

    Add POST users endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,30 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task = new Task({
-    description: 'Buy milk',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
 })
