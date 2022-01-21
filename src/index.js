7ac8382976695cbd249b43927d88965d9a0c27f7
zakve <zaklas.martin@gmail.com>
2022-01-21 14:27:46 +0100

    Add patch user endpoint

--- src/index.js
+++ src/index.js
@@ -42,6 +42,27 @@ app.post('/users', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
