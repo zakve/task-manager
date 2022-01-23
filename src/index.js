f3e341ad5fd4ddab2350e3553d310840df3c5725
zakve <zaklas.martin@gmail.com>
2022-01-23 15:23:07 +0100

    Add delete user endpoint

--- src/index.js
+++ src/index.js
@@ -63,6 +63,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        return res.status(500).send()
+    }
+})
+
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
