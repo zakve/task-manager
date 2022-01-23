79a73fdb2f6efdf6fe905b2c505b4b407bac205c
zakve <zaklas.martin@gmail.com>
2022-01-23 15:28:51 +0100

    Add delete task endpoint

--- src/index.js
+++ src/index.js
@@ -96,7 +96,7 @@ app.get('/tasks/:id', async (req, res) => {
 
         res.send(task)
     } catch (error) {
-        return res.status(500).send()
+        return res.status(500).send(error)
     }
 })
 
@@ -129,6 +129,19 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task)
+            return res.status(404).send()
+
+        res.send(task)
+    } catch (error) {
+        return res.status(500).send(error)
     }
 })
 
