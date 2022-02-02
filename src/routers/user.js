c7bb4811a3e38d530d2c90fee516858cc868b34b
zakve <zaklas.martin@gmail.com>
2022-02-02 14:20:41 +0100

    Add logout and logoutAll endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -18,7 +18,33 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users/:id', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error)
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error)
+    }
+})
+
+router.get('/users/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
