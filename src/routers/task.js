0fb9796f5137f986e5edcd8b3a023e57d67f0399
zakve <zaklas.martin@gmail.com>
2022-02-07 13:55:18 +0100

    Add filtering and pagination for tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,10 +3,25 @@ const router = new express.Router()
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 
+// GET /tasks?completed=true
+// GET second page /tasks?limit=10&skip=10
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
         //const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (error) {
         res.status(500).send()
