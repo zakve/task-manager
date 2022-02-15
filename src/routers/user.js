faca83fdb4ad37914bdb110087f73828eca4495f
zakve <zaklas.martin@gmail.com>
2022-02-15 17:17:31 +0100

    Add avatar validation

--- src/routers/user.js
+++ src/routers/user.js
@@ -85,7 +85,16 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000 // bytes 1MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
