8f1296661acaaa77b9cb8a1b15cf0029bdd1cc8b
zakve <zaklas.martin@gmail.com>
2022-03-05 15:47:45 +0100

    Add clear DB before test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,10 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+beforeEach(async () => {
+    await User.deleteMany()
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
