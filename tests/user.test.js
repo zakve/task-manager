a7d8ba884f9cefddcaf1ca09709ed4777daa8a7a
zakve <zaklas.martin@gmail.com>
2022-03-05 16:11:54 +0100

    Add login tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,14 +2,27 @@ const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOne = {
+    name: 'Mike',
+    email: 'miketest@example.com',
+    password: 'test123'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+    await new User(userOne).save()
 })
 
-test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
-        name: 'M',
-        email: 'zaklas@centrum.cz',
-        password: 'test2020'
-    }).expect(201)
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'wrongPassword'
+    }).expect(400)
 })
